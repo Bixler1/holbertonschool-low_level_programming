bdb9a56fd755e1461600f608de5119ef228cc089
Bixler1 <bixler71@gmail.com>
2026-07-08 12:30:36 +1000

    g

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
-
-main - Entry point
-Return: Always 0*/
+ *main - Entry poin
+ *
+ *Return: Always 0
+*/
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
