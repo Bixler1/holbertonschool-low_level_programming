0570aa39b8b0a3f6b40fe50edc0d943b63b001b8
Bixler1 <bixler71@gmail.com>
2026-07-13 11:21:34 +1000

    g

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	/* Extract the last digit using the modulo operator */
+	last_digit = n % 10;
+
+	/* Print the leading string common to all outputs */
+	printf("Last digit of %d is %d ", n, last_digit);
+
+	/* Evaluate the conditions and append the appropriate text */
+	if (last_digit > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (last_digit == 0)
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
+	return (0);
+}
