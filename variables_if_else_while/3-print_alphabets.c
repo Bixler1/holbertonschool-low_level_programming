8043d763cb450f28a7a9c77e11e604d1895d5d80
Bixler1 <bixler71@gmail.com>
2026-07-14 11:12:46 +1000

    g

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,17 +13,13 @@ int main(void)
 	
 	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
-<<<<<<< HEAD
-		putchar(lowercase);
+	  putchar(lowercase);
 	}
-
-=======
-		putchar (lowercase);
+	putchar (lowercase);
 	}
->>>>>>> 2f54db95d763d191048a66258fa735e058ed618f
 	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	{
-		putchar(uppercase);
+	  putchar(uppercase);
 	}
 	putchar('\n');
 	return (0);
