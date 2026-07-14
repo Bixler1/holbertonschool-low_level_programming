1d9178a4486f41f66c1ebfa18749cd97b2ca82a2
Bixler1 <bixler71@gmail.com>
2026-07-14 11:01:42 +1000

    Update 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,18 +5,20 @@
  * Description: Prints lowercase alphabet, uppercase alphabet, and a newline.
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char lowercase;
 	char uppercase;
+	
 	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 	{
-		putchar(lowercase);
+		putchar (lowercase);
 	}
 	for(uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	{
 		putchar(uppercase);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
