29a5c865d79f0af382aaade707ab0e283dc74713
Bixler1 <bixler71@gmail.com>
2026-07-15 14:35:43 +1000

    g

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,20 +8,22 @@
  */
 int main(void)
 {
-  int hex = 0;
-  while(hex < 16)
-    {
-      if (hex < 10)
-	{
-	  putchar(hex + '0');
-	}
-      else
-	{
-	  putchar((hex - 10) + 'a');
-	}
-      hex++;
-    }
-  putchar('\n');
+	int hex = 0;
 
-  return (0);
+	while (hex < 16)
+	{
+		if (hex < 10)
+		{
+			putchar(hex + '0');
+		}
+		else
+		{
+			putchar((hex - 10) + 'a');
+		}
+		hex++;
+	}
+	putchar('\n');
+
+	return (0);
 }
+
