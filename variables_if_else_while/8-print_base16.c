da92f1b967c57cd3b6d323b1867876b15954b79a
Bixler1 <bixler71@gmail.com>
2026-07-15 14:31:52 +1000

    g

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all numbers of base 16 in lowercase,
+ * followed by a new line, using only putchar three times.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int hex = 0;
+  while(hex < 16)
+    {
+      if (hex < 10)
+	{
+	  putchar(hex + '0');
+	}
+      else
+	{
+	  putchar((hex - 10) + 'a');
+	}
+    }
+  num++;
+}
