6f5e0ef59b2e34178eed0e1b1ed22bc054864391
Bixler1 <bixler71@gmail.com>
2026-07-20 14:18:06 +1000

    g

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet - alphabet
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
